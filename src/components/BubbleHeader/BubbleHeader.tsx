9aceb9cef90c37a4721b58220409d0d09733eb02
Jack Rifkin <jack.rifkin@icloud.com>
2025-01-14 20:10:53 -0500

    added hover effects

--- src/components/BubbleHeader/BubbleHeader.tsx
+++ src/components/BubbleHeader/BubbleHeader.tsx
@@ -6,13 +6,12 @@ const SM_SCREEN_WIDTH = 576;
 const MD_SCREEN_WIDTH = 768;
 const SM_BUBBLE_SIZE = 12;
 const REG_BUBBLE_SIZE = 20;
+const MIN_MOUSE_PROXIMITY = 100;
 
 const BubbleHeader = () => {
   const svgRef = useRef<SVGSVGElement>(null);
   const bubblesRef = useRef<HTMLDivElement>(null);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [finishedIntroAnimation, setFinishedIntroAnimation] =
-    useState<boolean>(false);
 
   useEffect(() => {
     if (svgRef.current && bubblesRef) {
@@ -57,11 +56,41 @@ const BubbleHeader = () => {
                 bubble.style.animation = `bob ${
                   bubbleSize === REG_BUBBLE_SIZE ? 1.5 : 1.7
                 }s infinite alternate ease-in-out`;
-                setFinishedIntroAnimation(true);
+                bubble.style.transition = `transform ${1.3 + timing}s ease-out`;
               };
               bubble.addEventListener("transitionend", listener);
               return () =>
                 bubble.removeEventListener("transitionend", listener);
+            });
+
+            listeners.push(() => {
+              const handleMouseMove = (event: MouseEvent) => {
+                const bubbleRect = bubble.getBoundingClientRect();
+                const bubbleRadius = bubbleSize / 2;
+                const mouseX = event.clientX - bubbleRect.left + bubbleRadius;
+                const mouseY = event.clientY - bubbleRect.top + bubbleRadius;
+                const distance = Math.sqrt(mouseX * mouseX + mouseY * mouseY);
+
+                if (distance > MIN_MOUSE_PROXIMITY) return;
+                const dx = bubbleRadius - mouseX;
+                const dy = bubbleRadius - mouseY;
+
+                const angle = Math.atan2(dy, dx);
+                const force = MIN_MOUSE_PROXIMITY - distance;
+                const moveX = Math.cos(angle) * force;
+                const moveY = Math.sin(angle) * force;
+
+                bubble.style.animation = "";
+                bubble.style.transition = "transform 0.3s ease-out";
+                bubble.style.transform = `translate(${moveX}px, ${moveY}px)`;
+                setTimeout(() => {
+                  bubble.style.transition = "transform 0.3s ease-in-out";
+                  bubble.style.transform = "translate(0, 0)";
+                }, 300);
+              };
+              window.addEventListener("mousemove", handleMouseMove);
+              return () =>
+                window.removeEventListener("mousemove", handleMouseMove);
             });
           }
         }
@@ -73,7 +102,7 @@ const BubbleHeader = () => {
         listeners.forEach((removeListener) => removeListener());
       };
     }
-  }, [svgRef, bubblesRef, screenWidth, finishedIntroAnimation]);
+  }, [svgRef, bubblesRef, screenWidth]);
 
   useEffect(() => {
     const handleResize = () => {
